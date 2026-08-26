426477df343be4b5ac8b3a9dfcae1c62394b9124
Mohamed Sami <mohamedsamy1205@gmail.com>
2026-08-26 09:10:59 +0300

    fix: fix wrong import

--- app/grpc/detection_pb2_grpc.py
+++ app/grpc/detection_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import detection_pb2 as detection__pb2
+from . import detection_pb2 as detection__pb2
 
 GRPC_GENERATED_VERSION = '1.83.0'
 GRPC_VERSION = grpc.__version__
